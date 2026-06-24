8699d92094d2e7b09e2d802f0ac2ead892a65212
fian2504 <l200160138@student.ums.ac.id>
2026-06-24 23:52:10 +0700

    Initial commit from Create Next App

---
+++ src/lib/supabaseServer.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
+
+// Server-side only client
+export const supabaseServer = createClient(supabaseUrl, supabaseAnonKey);
